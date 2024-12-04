678c4add1075cbf45dbbc400a67025022c7c3af1
Mazen Hamed <72987078+Mazen7amed@users.noreply.github.com>
2024-12-04 17:17:30 +0200

    Update projectwescrabing.py

--- projectwescrabing.py
+++ projectwescrabing.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver import FirefoxOptions
 import os, sys
 
 @st.cache_resource
