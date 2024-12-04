7f00443ec7bbc815159a655a6c015262954c3069
Mazen Hamed <72987078+Mazen7amed@users.noreply.github.com>
2024-12-04 17:22:32 +0200

    Update projectwescrabing.py

--- projectwescrabing.py
+++ projectwescrabing.py
@@ -8,30 +8,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver import FirefoxOptions
-import os, sys
 
+
+
+def init_driver(service):
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', False)
+    firefox_profile.set_preference("media.volume_scale", "0.0")
+    firefox_profile.set_preference("dom.webnotifications.enabled", False)
+    URL = ""
+    TIMEOUT = 20
+
+    st.title("Test Selenium")
+
+    firefoxOptions = Options()
+    firefoxOptions.add_argument("--headless")
+    driver = webdriver.Firefox(options=firefoxOptions)
+    driver.get(URL)
+    return driver
 
 def scrape_jumia():
-    #installff()
-    opts = FirefoxOptions()
-    opts.add_argument("--headless")
-    driver = webdriver.Firefox(options=opts)
+    driver = init_driver(service)
     driver.get("https://www.jumia.com.eg/")
-    wait = WebDriverWait(driver, 30)  # Increase wait time to 30 seconds
-    try:
-        # Attempt to click the popup or other element
-        click1 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".cls")))
-        click1.click()
-    except TimeoutException:
-        print("Popup not found or could not be clicked")
-    
+    wait = WebDriverWait(driver, 10)
+    click1 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".cls")))
+    click1.click()
     search_box = driver.find_element(By.CSS_SELECTOR, "#fi-q")
     search_box.send_keys("smart watches")
     search_button = driver.find_element(By.CSS_SELECTOR, "button.-mls")
     search_button.click()
-    
     wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 
         "div.-paxs.row._no-g._4cl-3cm-shs article.prd._fb.col.c-prd")))
     titles = driver.find_elements(By.CSS_SELECTOR, 
@@ -64,38 +69,42 @@ def scrape_jumia():
     })
     return df
 
+def main():
+    st.title("Jumia Product Scraper")
+    st.subheader("We will scrape many products and choose the best product of best price and best discount ")
+    if st.button("Scrape now.."):
+        with st.spinner("Scraping data from Jumia..."):
+            df = scrape_jumia()
 
-st.title("Jumia Product Scraper")
-st.subheader("We will scrape many products and choose the best product of best price and best discount ")
-if st.button("Scrape now.."):
-    with st.spinner("Scraping data from Jumia..."):
-        df = scrape_jumia()
+        if df.empty:
+            st.warning("No data scraped. Please check the website or your scraping logic.")
+        else:
+            st.success("Scraping completed successfully!")
+            st.dataframe(df)
+    st.sidebar.title("Navigations")
+    st.sidebar.markdown("Created by [Youssef Shady](https://www.facebook.com/share/18MJH5gqat/?mibextid=LQQJ4d)")
+    st.sidebar.image("jumiaimage.png")
+    c1 = st.sidebar.selectbox("select an option..", ["EDA","Insights"])
+    df = scrape_jumia()
+    if c1 == "EDA":
+        c2 = st.sidebar.radio("select chart", ["Bar chart" , "Scatter chart"])
+        if c2 == "Scatter chart":
+            st.subheader("Prices")
+            sc1 = px.scatter(df, x= "Price" , y= "Old Price", color="Discount")
+            st.plotly_chart(sc1)
+            st.subheader("Discounts")
+            sc2 = px.scatter(df , x= "Old Price" , y= "Discount", color="Discount")
+            st.plotly_chart(sc2)
+        if c2 == "Bar chart":
+            st.subheader("Prices")
+            br1 = px.bar(df, x="Price" , y= "Old Price", color= "Discount")
+            st.plotly_chart(br1)
+            st.subheader("Discounts")
+            br2 = px.bar(df , x= "Old Price" , y= "Discount", color= "Discount")
+            st.plotly_chart(br2)
+    elif c1 == "Insights":
+        st.subheader("""1) The comparison between the current price and the old price highlights the level of price reductions. A significant difference indicates a notable price drop, which could attract cost-conscious customers. Products with a large gap between the old and current price are more likely to appeal as value-for-money items. 2) Items with visible discounts and significant old price reductions are likely part of a sales strategy to clear inventory or promote specific products. Products with minimal price differences or no discounts may cater to premium segments or represent newly launched items.""")
+if __name__ == "__main__":
+    main()
 
-    if df.empty:
-        st.warning("No data scraped. Please check the website or your scraping logic.")
-    else:
-        st.success("Scraping completed successfully!")
-        st.dataframe(df)
-st.sidebar.title("Navigations")
-st.sidebar.markdown("Created by [Youssef Shady](https://www.facebook.com/share/18MJH5gqat/?mibextid=LQQJ4d)")
-st.sidebar.image("jumiaimage.png")
-c1 = st.sidebar.selectbox("select an option..", ["EDA","Insights"])
-df = scrape_jumia()
-if c1 == "EDA":
-    c2 = st.sidebar.radio("select chart", ["Bar chart" , "Scatter chart"])
-    if c2 == "Scatter chart":
-        st.subheader("Prices")
-        sc1 = px.scatter(df, x= "Price" , y= "Old Price", color="Discount")
-        st.plotly_chart(sc1)
-        st.subheader("Discounts")
-        sc2 = px.scatter(df , x= "Old Price" , y= "Discount", color="Discount")
-        st.plotly_chart(sc2)
-    if c2 == "Bar chart":
-        st.subheader("Prices")
-        br1 = px.bar(df, x="Price" , y= "Old Price", color= "Discount")
-        st.plotly_chart(br1)
-        st.subheader("Discounts")
-        br2 = px.bar(df , x= "Old Price" , y= "Discount", color= "Discount")
-        st.plotly_chart(br2)
-elif c1 == "Insights":
-    st.subheader("""1) The comparison between the current price and the old price highlights the level of price reductions. A significant difference indicates a notable price drop, which could attract cost-conscious customers. Products with a large gap between the old and current price are more likely to appeal as value-for-money items. 2) Items with visible discounts and significant old price reductions are likely part of a sales strategy to clear inventory or promote specific products. Products with minimal price differences or no discounts may cater to premium segments or represent newly launched items.""")
+
