477d3f91313e89a80714188d7722337fffa05c66
Mazen Hamed <72987078+Mazen7amed@users.noreply.github.com>
2024-12-04 15:58:55 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Firefox
+apt-get update
+apt-get install -y firefox-esr
+
+# Install geckodriver
+curl -L https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz -o geckodriver.tar.gz
+tar -xvzf geckodriver.tar.gz
+mv geckodriver /usr/local/bin/
+
+# Clean up
+rm geckodriver.tar.gz
